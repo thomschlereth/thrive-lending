55cd099f4ae51a45850d8c2479d1a88321022c97
thompickett <thompickett@gmail.com>
2016-05-18 17:24:22 -0600

    Update seed fiel to change los and lfs to inactive when contract is created (#73)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,11 @@ class Seed
 
   def create_users
     puts "Creating Users"
-    create_kami_admin
-    create_mark_admin
+    create_andrew_admin
     puts "Created Admin"
-    create_thom
+    create_josh
     puts "Created Standard Default User"
-    until User.count == 50 do
+    until User.count == 100 do
       begin
         User.create!(first_name: username, last_name: Faker::Name.last_name, description: Faker::Commerce.department,
           email: Faker::Internet.email, city: Faker::Address.city, state: Faker::Address.state,
@@ -25,6 +24,7 @@ class Seed
           email: Faker::Internet.email, city: Faker::Address.city, state: Faker::Address.state,
           zipcode: Faker::Address.zip, password: "password", image_path: photo_url,
           address: Faker::Address.street_address, role: 0, username: username.downcase)
+        puts "User Rescued"
       end
     end
     puts "Created Random Standard Users"
@@ -32,11 +32,7 @@ class Seed
 
   def create_loan_offers
     puts "Creating Loan Offers"
-    LoanOffer.create(amount: rand(50..10000),
-     rate: "#{rand(3..20)}.#{rand(0..99)}".to_f,
-     term: rand(3..12),
-     user_id: 3)
-    until LoanOffer.count == 100 do
+    until LoanOffer.count == 50 do
       begin
         user = User.find(rand(3..50))
         loan = user.loan_offers.create!(amount: rand(50..10000),
@@ -55,7 +51,7 @@ class Seed
 
   def create_loan_requests
     puts "Creating Loan Requests"
-    until LoanRequest.count == 150 do
+    until LoanRequest.count == 50 do
       begin
         user = User.find(rand(3..50))
         loan = user.loan_requests.create!(amount: rand(50..10000),
@@ -75,38 +71,32 @@ class Seed
   def create_request_contract(user_id, loan_id)
     status = ["Pending", "Approved", "Paid"]
     contract = Contract.create!(lender_id: rand(3..50), borrower_id: user_id, status: status.sample, loan_request_id: loan_id)
+    contract.loan_request.update(active: false)
   end
 
   def create_offer_contract(user_id, loan_id)
     status = ["Pending", "Approved", "Paid"]
     contract = Contract.create!(borrower_id: rand(3..50), lender_id: user_id, status: status.sample, loan_offer_id: loan_id)
+    contract.loan_offer.update(active: false)
   end
 
 
   private
 
-  def create_kami_admin
-    User.create!(first_name: "Kami", last_name: "Boers", image_path: photo_url,
-      email: "kami@kami.kami", city: "Denver", state: "CO",
-      zipcode: "80229", password: "password", description: Faker::Commerce.department,
-      address: "The Dungeon where there is no sunlight",
-      role: 1, username: "kami")
-  end
-
-  def create_mark_admin
-    User.create!(first_name: "Mark", last_name: "Miranda", image_path: photo_url,
-      email: "mark@mark.mark", city: "Denver", state: "CO",
+  def create_andrew_admin
+    User.create!(first_name: "Andrew", last_name: "Carmer", image_path: photo_url,
+      email: "Andrew@andrew.andrew", city: "Denver", state: "CO",
       zipcode: "80229", password: "password", description: Faker::Commerce.department,
       address: "The basement flooding from students tears",
-      role: 1, username: "mark")
+      role: 1, username: "andrew")
   end
 
-  def create_thom
-    User.create!(first_name: "Thom", last_name: "Pickett", image_path: photo_url,
-      email: "thom@thom.thom", city: "Denver", state: "CO",
+  def create_josh
+    User.create!(first_name: "Josh", last_name: "Mejia", image_path: photo_url,
+      email: "josh@josh.josh", city: "Denver", state: "CO",
       zipcode: "80229", password: "password", description: Faker::Commerce.department,
-      address: "The Purgatory of waiting to see if we pass or fail",
-      role: 0, username: "thom")
+      address: "The Purgatory that is this project waiting to see if we pass or fail",
+      role: 0, username: "josh")
   end
 
   def username
